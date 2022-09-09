63eef518ab1de0f4358c34c34f2f08b1bfe5f758
Moritz Lahann <moritz.lahann@akra.de>
2022-09-09 14:47:00 +0200

    aspect: genre counts

--- statistics.py
+++ statistics.py
@@ -27,7 +27,15 @@ for book_path in tqdm(books):
 
 books_df = pd.json_normalize(all_books)
 
+# Find frequent genres
 pd.Series(np.concatenate(books_df["genres"])).value_counts()[:20].plot(
     kind="barh", title="20 Most Frequent Genres"
 )
 plt.savefig("figures/genre_frequencies.png", bbox_inches="tight")
+
+# Count books with genres we don't want
+genre_df = books_df.explode(column="genres")
+print("Lyrik: ", len(genre_df[genre_df["genres"] == "Lyrik"]))
+print("Dramatik: ", len(genre_df[genre_df["genres"] == "Dramatik"]))
+print("Anthologie: ", len(genre_df[genre_df["genres"] == "Anthologie"]))
+print("Fabeln: ", len(genre_df[genre_df["genres"] == "Fabeln"]))
